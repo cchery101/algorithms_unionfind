fb1c1a8e86a0a7e59706c68199186da293f31631
Simon Bowly <simon.bowly@gmail.com>
2014-06-27 17:51:16 +1000

    style guide

--- PercolationStats.java
+++ PercolationStats.java
@@ -1,22 +1,20 @@
 
 import java.io.*;
-import java.util.Arrays;
-import java.lang.*;
 
 public class PercolationStats {
-    private double threshold[];
-    double sqrttests;
+    private double[] threshold;
+    private int numtests;
     // perform T independent computational experiments on an N-by-N grid
     public PercolationStats(int N, int T)
     {
-        sqrttests = Math.sqrt(T);
+        numtests = T;
         Percolation myobj;
-        if (N <=0 || T <= 0) throw new java.lang.IllegalArgumentException();
+        if (N <= 0 || T <= 0) throw new java.lang.IllegalArgumentException();
         threshold = new double[T];
         for (int test = 0; test < T; test++)
         {
             myobj = new Percolation(N);
-            while (myobj.percolates() == false)
+            while (!myobj.percolates())
             {
                 // open random sites while there is no percolation
                 myobj.open(StdRandom.uniform(N) + 1, StdRandom.uniform(N) + 1);
@@ -36,16 +34,25 @@ public class PercolationStats {
     // sample standard deviation of percolation threshold
     public double stddev() { return StdStats.stddev(threshold); }
     // returns lower bound of the 95% confidence interval
-    public double confidenceLo() { return mean() - 1.96 * stddev() / sqrttests; }
+    public double confidenceLo() { return mean() - 1.96 * stddev()
+        / Math.sqrt(numtests); }
     // returns upper bound of the 95% confidence interval
-    public double confidenceHi() { return mean() + 1.96 * stddev() / sqrttests; }
+    public double confidenceHi() { return mean() + 1.96 * stddev()
+        / Math.sqrt(numtests); }
    
     // test client
     public static void main(String[] args)
     {
-        PercolationStats testobj = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
-        System.out.println(String.format("mean =                    %15.15f", testobj.mean()));
-        System.out.println(String.format("stddev =                  %15.15f", testobj.stddev()));
-        System.out.println(String.format("95%% confidence interval = %15.15f, %15.15f", testobj.confidenceLo(), testobj.confidenceHi()));
+        PercolationStats testobj = new PercolationStats(
+            Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        System.out.println(String.format(
+            "mean =                    %15.15f",
+            testobj.mean()));
+        System.out.println(String.format(
+            "stddev =                  %15.15f",
+            testobj.stddev()));
+        System.out.println(String.format(
+            "95%% confidence interval = %15.15f, %15.15f",
+            testobj.confidenceLo(), testobj.confidenceHi()));
     }
 }
