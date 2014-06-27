9f16ac734f17fa6f44f8045848fc8fa9ea1387ca
Simon Bowly <simon.bowly@gmail.com>
2014-06-27 12:34:07 +1000

    implemeneted weighted union

--- QuickUnionUF.java
+++ QuickUnionUF.java
@@ -1,30 +1,43 @@
 
-public class QuickFindUF
+public class QuickUnionUF
 {
     // id[i] is the connected group of element i
     private int[] id;
+    private int[] sz;
 
     // constructor: initialise group of size N with no connections
-    public QuickFindUF(int N)
+    public QuickUnionUF(int N)
     {
         // initialise array member (pointer)
         id = new int[N];
+        sz = new int[N];
         // give each element its own group (disconnected)
         for (int i = 0; i < N; i++)
+        {
             id[i] = i;
+            sz[i] = 1;
+        }
     }
 
-    // test for connectedness: return True if members p & q are connected
-    public boolean connected(int p, int q)
-    { return id[p] == id[q]; }
+    // return p's root, by moving recursively up through id tree
+    private int root(int p)
+    {
+        while (p != id[p]) p = id[p];
+        return p;
+    }
 
-    // connect two members by creating the union of their groups
+    // test for connectedness: return true if members p & q have same root
+    public boolean connected(int p, int q)
+    { return root(p) == root(q); }
+
+    // connect two members by connecting their roots
     public void union(int p, int q)
     {
-        int p_group = id[p];
-        int q_group = id[q];
-        // change all p_group members to q_group
-        for (int i = 0; i < id.length; i++)
-            if (id[i] == p_group) id[i] = q_group;
+        int root_p = root(p);
+        int root_q = root(q);
+        if (root_p == root_q) return;
+        // connect the smaller tree under the bigger tree
+        if (sz[p] < sz[q])  { id[root(p)] = root(q); sz[q] += sz[p]; }
+        else                { id[root(q)] = root(p); sz[p] += sz[q]; }
     }
 }
--- QuickUnionUF_Test.java
+++ QuickUnionUF_Test.java
@@ -5,12 +5,13 @@ public class QuickUnionUF_Test
     {
         System.out.println("Hello World!");
         QuickUnionUF myobj = new QuickUnionUF(10);
-        System.out.println(myobj.connected(1,2));
-        System.out.println(myobj.connected(1,3));
+        System.out.println(myobj.connected(1,1));  // true
+        System.out.println(myobj.connected(1,2));  // false
+        System.out.println(myobj.connected(1,3));  // false
         myobj.union(1,2);
-        System.out.println(myobj.connected(1,2));
-        System.out.println(myobj.connected(1,3));
+        System.out.println(myobj.connected(1,2));  // true - direct
+        System.out.println(myobj.connected(1,3));  // false
         myobj.union(2,3);
-        System.out.println(myobj.connected(1,3));
+        System.out.println(myobj.connected(1,3));  // true - transitive
     }
 }
